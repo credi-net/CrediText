d22bc432b2276dd61595fcafa99a5a388588497f
IslamHisham <sulum821991@gmail.com>
2026-05-20 15:06:53 -0400

    fixing topic modeler method

--- domain_sampler/sampler.py
+++ domain_sampler/sampler.py
@@ -132,26 +132,26 @@ class DomainSampler():
                         "text":self.clean_text(soup.get_text(separator=" ", strip=True))})
         return articles_text
     
-def topic_analysis(self, urls:list[str], articles:list)-> pd.DataFrame:
-    """
-    Preprocesses the HTML content of articles from a domain and returns topic with their relative urls ordered by 
-    the number of words of their corresponding article content.
+    def topic_analysis(self, urls:list[str], articles:list)-> pd.DataFrame:
+        """
+        Preprocesses the HTML content of articles from a domain and returns topic with their relative urls ordered by 
+        the number of words of their corresponding article content.
 
-    :param urls: list fo the urls sample from limited population theory
-    :param articles: list of articles HTML content
-    """ 
-    cleaned_articles = [art['text'] for art in tqdm(self.preprocess_html(articles))]
-    art_lens = [len(art.split()) for art in cleaned_articles]
-    topics, probs = topic_model.transform(cleaned_articles)
-    analysis_df = pd.DataFrame({'url':urls, 'article_word_num':art_lens, 'topic' : topics, 'prob': probs})
-    # 1. Sort the dataframe by the number of words in ascending order
-    analysis_df = analysis_df.sort_values(by="article_word_num", ascending=True)
+        :param urls: list fo the urls sample from limited population theory
+        :param articles: list of articles HTML content
+        """ 
+        cleaned_articles = [art['text'] for art in tqdm(self.preprocess_html(articles))]
+        art_lens = [len(art.split()) for art in cleaned_articles]
+        topics, probs = topic_model.transform(cleaned_articles)
+        analysis_df = pd.DataFrame({'url':urls, 'article_word_num':art_lens, 'topic' : topics, 'prob': probs})
+        # 1. Sort the dataframe by the number of words in ascending order
+        analysis_df = analysis_df.sort_values(by="article_word_num", ascending=True)
 
-    # 2. Group by topic and aggregate the URLs into a list
-    result_df = (
-        analysis_df.groupby("topic")["url"]
-        .apply(list)
-        .reset_index(name="ordered_urls"))
-    return result_df
+        # 2. Group by topic and aggregate the URLs into a list
+        result_df = (
+            analysis_df.groupby("topic")["url"]
+            .apply(list)
+            .reset_index(name="ordered_urls"))
+        return result_df
 
 
