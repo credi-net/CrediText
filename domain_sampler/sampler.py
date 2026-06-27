2f385121e74dcf904136fd8e9fbf7592fd760b34
IslamHisham <sulum821991@gmail.com>
2026-06-27 12:53:07 -0400

    adding the topic similarity calculation method based only on embeddings

--- domain_sampler/sampler.py
+++ domain_sampler/sampler.py
@@ -63,6 +63,7 @@ class DomainSampler():
         try:
             self.topic_model = BERTopic.load(bert_model_path, embedding_model = self.embedding_model)
             self.topic_representations = self.topic_model.get_topic_info() #shows the representative words per topic
+            self.topic_embeddings = self.topic_model.topic_embeddings_  #embeddings ordered by the number of topic
         except:
             raise ValueError(f"BERTopic model could not be loaded from the path: {bert_model_path}. Please check the path and try again.")
     @staticmethod
@@ -89,7 +90,7 @@ class DomainSampler():
 
         return math.ceil(n)
 
-    def get_gaussian_weights(self, input_list: list)->np.ndarray:
+    def get_gaussian_weights(self, input_list: list, art_num: int)->np.ndarray:
         """
         Generates a list of Gaussian weights corresponding to the input list.
         The weights peak at the center and decrease toward the edges.
@@ -108,6 +109,8 @@ class DomainSampler():
         # 3. Normalize so all weights add up to 1.0 (highly recommended)
         weights /= np.sum(weights)
         
+        # normalize weights by articles per topic relevant to the total number of articles
+        weights *= (n / art_num)
         return weights
 
   
@@ -212,5 +215,25 @@ class DomainSampler():
             analysis_df.groupby("topic")["url"]
             .apply(list)
             .reset_index(name="ordered_urls")) 
-        result_df['url_weights'] = (1/len(result_df))*result_df.ordered_urls.apply(self.get_gaussian_weights)       
+        result_df['url_gaussian_weights'] = result_df.ordered_urls.apply(lambda x: self.get_gaussian_weights(x, len(analysis_df)))       
         return [analysis_df, result_df]
+    def get_topic_sim_based_on_embeddings(self, input_embeddings:np.ndarray)->np.ndarray:
+        """
+        Predicts the topics based on cosine similarity for each embedding based on the pre-trained BERTopic model.
+
+        :param input_embeddings: A list of embeddings for which to predict topics
+        :return: A list of predicted topic similarity scores corresponding to the input embeddings
+        """
+        # 1. Compute dot product between matrix and vector -> shape (10,)
+        dot_product = np.dot(self.topic_embeddings, input_embeddings)
+
+        # 2. Compute L2 norm (magnitude) of the single vector -> scalar
+        norm_vector = np.linalg.norm(input_embeddings)
+
+        # 3. Compute L2 norm of each row in the matrix -> shape (10,)
+        norm_matrix = np.linalg.norm(input_embeddings, axis=1)
+
+        # 4. Divide dot product by the product of the norms
+        cosine_sim = dot_product / (norm_vector * norm_matrix)
+
+        return cosine_sim
