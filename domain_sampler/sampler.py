4861eeda9817e20b0c68d5544dec0caacb9a3e6f
IslamHisham <sulum821991@gmail.com>
2026-07-27 00:33:20 -0400

    adding the exact centroid embeddings
    Updated import statements and improved the initialization of the DomainSampler class. Changed the loading of topic representations from Excel to CSV format.

--- domain_sampler/sampler.py
+++ domain_sampler/sampler.py
@@ -82,8 +82,8 @@ class DomainSampler():
             except:
                 raise ValueError(f"BERTopic model could not be loaded from the path: {bert_model_path}. Please check the path and try again.")
         print(f"loading topic information and embedding: ...")
-        self.topic_representations = pd.read_excel(bert_model_path+"/topic_info.xlsx") #shows the representative words per topic
-        self.topic_embeddings = self.topic_representations.embedding.progress_apply(lambda x: np.fromstring(x.strip("[]"),sep=' ')).to_list()
+        self.topic_representations = pd.read_csv(bert_model_path+"/topic_info.csv") #shows the representative words per topic
+        self.topic_embeddings = self.topic_representations.embedding.progress_apply(lambda x:np.fromstring(x.strip('[').strip(']'), dtype=float, sep=" "))
         self.topic_ids = self.topic_representations.Topic.to_list()
     @staticmethod
     def get_min_sample_size(pop_size: int, confidence: float, margin_error: float)->int:
